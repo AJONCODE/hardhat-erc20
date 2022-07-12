2cd02acceebc78b8701009befa9c5118e3eedf06
AJONCODE <ajoncode@gmail.com>
2022-07-12 13:58:45 +0530

    Enabling Transfers

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -6,6 +6,10 @@ contract ERC20 {
     string public name;
     string public symbol;
 
+    // Since the mapping is of type public, hence a view function balanceOf
+    // is automatically generated behind the scenes.
+    mapping(address => uint256) public balanceOf;
+
     constructor(string memory _name, string memory _symbol) {
         name = _name;
         symbol = _symbol;
@@ -13,5 +17,24 @@ contract ERC20 {
 
     function decimals() external pure returns (uint8) {
         return 18;
-    }   
+    }
+
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool)
+    {
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        uint256 senderBalance = balanceOf[msg.sender];
+
+        require(
+            senderBalance >= amount,
+            "ERC20: transfer amount exceeds balance"
+        );
+
+        balanceOf[msg.sender] = senderBalance - amount;
+        balanceOf[recipient] += amount;
+
+        return true;
+    }
 }
